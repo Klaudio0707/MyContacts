d4f07d5f7c4a1af35acc16676fcdbabcd39d89cc
Cláudio Roberto <claudioroberto0707@outlook.com>
2025-06-12 00:13:11 -0300

    inclusão do icone e botao ao lado do componente findContacts

--- src/Components/FindContacts/FindContacts.tsx
+++ src/Components/FindContacts/FindContacts.tsx
@@ -1,10 +1,22 @@
+import { useRef } from "react";
 import styles from "./styles.module.css"
+import { MdOutlineContentPasteSearch } from "react-icons/md";
 
 
-const FindContacts = () => {
+
+const FindContacts:React.FC = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const handleIconClick = () => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  };
   return (
     <section className={styles.container_Input}>
-      <input type="text" placeholder="Pesquise um Contato" className={styles.input_Find}/>
+      <input type="text"  ref={inputRef} placeholder="Pesquise um Contato" className={styles.input_Find}/>
+      <button className={styles.search_Btn} onClick={handleIconClick}   aria-label="Pesquisar Contato">
+      <MdOutlineContentPasteSearch className={styles.search_Icone}  size={35}/>
+      </button>
     </section>
   )
 }
